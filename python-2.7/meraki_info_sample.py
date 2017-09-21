baa4912443b162ce9bebfd0b83c9c5ea50f4f468
sushmitapa <31866315+sushmitapa@users.noreply.github.com>
2017-09-21 13:52:24 -0700

    Update meraki_info_sample.py

--- python-2.7/meraki_info_sample.py
+++ python-2.7/meraki_info_sample.py
@@ -1,4 +1,4 @@
-api_key = 'abc12345'
+api_key = '7e20b680d69a9bc69da474fea68097fbf71c6baf'
 base_url = 'https://dashboard.meraki.com/api/v0'
 community_string = 'my_community_string'
 snmp_port = 12345
